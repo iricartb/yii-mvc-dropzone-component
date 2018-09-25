3ac7511dc09aa762f48240bf24a47b69a01f0faa
iricartb <iricartb@gmail.com>
2018-09-25 16:57:03 +0200

    Yii - MVC Dropzone Component - DB Interaction

--- protected/models/HeaderSliderImages.php
+++ protected/models/HeaderSliderImages.php
@@ -10,84 +10,84 @@
  */
 class HeaderSliderImages extends CDropzoneActiveRecord {
    
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return HeaderSliderImages the static model class
-	 */
-	public static function model($className=__CLASS__) {
-		return parent::model($className);
-	}
+   /**
+    * Returns the static model of the specified AR class.
+    * @param string $className active record class name.
+    * @return HeaderSliderImages the static model class
+    */
+   public static function model($className=__CLASS__) {
+      return parent::model($className);
+   }
 
-	/**
-	 * @return CDbConnection database connection
-	 */
-	public function getDbConnection() {
-		return Yii::app()->db_site;
-	}
+   /**
+    * @return CDbConnection database connection
+    */
+   public function getDbConnection() {
+      return Yii::app()->db_site;
+   }
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName() {
-		return 'header_slider_images';
-	}
+   /**
+    * @return string the associated database table name
+    */
+   public function tableName() {
+      return 'header_slider_images';
+   }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules() {
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('image, position', 'required'),
-			array('position', 'numerical', 'integerOnly'=>true),
+   /**
+    * @return array validation rules for model attributes.
+    */
+   public function rules() {
+      // NOTE: you should only define rules for those attributes that
+      // will receive user inputs.
+      return array(
+         array('image, position', 'required'),
+         array('position', 'numerical', 'integerOnly'=>true),
          array('image', 'length', 'max'=>45),
          
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, image, position', 'safe', 'on'=>'search'),
-		);
-	}
+         // The following rule is used by search().
+         // Please remove those attributes that should not be searched.
+         array('id, image, position', 'safe', 'on'=>'search'),
+      );
+   }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations() {
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array();
-	}
+   /**
+    * @return array relational rules.
+    */
+   public function relations() {
+      // NOTE: you may need to adjust the relation name and the related
+      // class name for the relations automatically generated below.
+      return array();
+   }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels() {
-		return array(
-			'id'=>'ID',
-			'image'=>'Image',
-			'position'=>'Position',
-		);
-	}
+   /**
+    * @return array customized attribute labels (name=>label)
+    */
+   public function attributeLabels() {
+      return array(
+         'id'=>'ID',
+         'image'=>'Image',
+         'position'=>'Position',
+      );
+   }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search() {
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+   /**
+    * Retrieves a list of models based on the current search/filter conditions.
+    * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+    */
+   public function search() {
+      // Warning: Please modify the following code to remove attributes that
+      // should not be searched.
 
-		$criteria = new CDbCriteria;
+      $criteria = new CDbCriteria;
 
-		$criteria->compare('id', $this->id);
-		$criteria->compare('image', $this->image, true);
-		$criteria->compare('position', $this->position);
+      $criteria->compare('id', $this->id);
+      $criteria->compare('image', $this->image, true);
+      $criteria->compare('position', $this->position);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+      return new CActiveDataProvider($this, array(
+         'criteria'=>$criteria,
+      ));
+   }
    
    public static function getHeaderSliderImage($nId) {
       return HeaderSliderImages::model()->find('id = ' . $nId);   
